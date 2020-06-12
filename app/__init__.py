6ac6907a978f09d4a8b7eaf376c8b66e260fe8cb
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-06-12 15:46:43 +0300

    add changes to start.sh

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import IMAGES, UploadSet,configure_uploads
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 mail = Mail()
@@ -19,6 +20,7 @@ photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
+    app.config['SECRET_KEY'] = 'secret'
     from .auth import auth as authentication_blueprint
     from .main import main as main_blueprint
 
---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY=secret
+
+export MAIL_USERNAME=midumbi.webdev@gmail.com
+export MAIL_PASSWORD=moringa81
+
+python3 manage.py server
