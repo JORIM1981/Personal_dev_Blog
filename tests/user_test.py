9d6ec1f29e057d1a1a5ab93a192ed27ad22d74c8
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-06-12 16:23:34 +0300

    add blog_test db

--- tests/user_test.py
+++ tests/user_test.py
@@ -4,7 +4,7 @@ from app.models import User
 
 class UserTest(unittest.TestCase):
     def setUp(self):
-        self.new_user = User(username='midumbi'email = "test@test.com" bio ='default bio',password='moringa81')
+        self.new_user = User(username='midumbi'email = "midumbi.webdev@gmail.com" bio ='default bio',password='moringa81')
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.password is not None)
